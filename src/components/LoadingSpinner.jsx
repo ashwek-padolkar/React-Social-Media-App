0ba7ea313fe85c2f757b49ea66d78db109829c1c
AshwekPadolkar <padolkarashwek11@gmail.com>
2024-08-10 01:59:12 +0530

    first commit

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,15 @@
+const LoadingSpinner = () => {
+  return (
+    <div className="d-flex justify-content-center">
+      <div
+        className="spinner-border spinner"
+        style={{ width: "5rem", height: "5rem" }}
+        role="status"
+      >
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
